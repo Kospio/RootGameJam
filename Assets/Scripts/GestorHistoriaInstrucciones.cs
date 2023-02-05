161c6f6b4d18b73dd068c586dca55e2ae4020e9e
MikelCarpioSitcom <55896197+Kospio@users.noreply.github.com>
2023-02-05 09:47:16 +0100

    Instruicciones hechas

---
+++ Assets/Scripts/GestorHistoriaInstrucciones.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement; 
+
+public class GestorHistoriaInstrucciones : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Sprite[] images;
+
+    public GameObject pantallaImagenGO;
+    public Image pantallaImagen; 
+
+    public int conteo;
+
+    public float duration;
+
+    public string nextScene;
+
+    public Canvas instrucciones;
+
+    public bool canbutton; 
+
+    private void Start()
+    {
+        instrucciones.gameObject.SetActive(false);
+
+        pantallaImagen = pantallaImagenGO.GetComponent<Image>(); 
+    }
+
+    public void preCambioHistoria()
+    {
+        StartCoroutine(CambiosHistoria());
+        canbutton = false; 
+    }
+
+    public IEnumerator CambiosHistoria()
+    {
+        float elapsedTime = 0;
+        float startValue = pantallaImagen.color.a;
+        float endValue = 0; 
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startValue, endValue, elapsedTime / duration);
+            pantallaImagen.color = new Color(pantallaImagen.color.r, pantallaImagen.color.g, pantallaImagen.color.b, newAlpha);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.2f);
+
+        conteo++;
+
+        if (conteo == images.Length)
+        {
+            SceneManager.LoadScene(nextScene); 
+        }
+
+        else if (conteo == images.Length - 1)
+        {
+            instrucciones.gameObject.SetActive(true);
+        }
+
+        else
+        {
+            pantallaImagen.sprite = images[conteo];
+
+            elapsedTime = 0;
+
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                float newAlpha = Mathf.Lerp(0, 1, elapsedTime / duration);
+                pantallaImagen.color = new Color(pantallaImagen.color.r, pantallaImagen.color.g, pantallaImagen.color.b, newAlpha);
+                yield return null;
+            }
+        }
+
+        canbutton = true; 
+    }
+}
